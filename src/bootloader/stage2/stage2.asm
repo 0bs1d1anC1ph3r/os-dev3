c97cf52dc2b46fd94882d28700b35c63b760441e
f76goat <indigolucas69@gmail.com>
2025-05-23 18:20:26 -0400

    HOLYSHITFUCKYESITWORKS

--- src/bootloader/stage2/stage2.asm
+++ src/bootloader/stage2/stage2.asm
@@ -3,16 +3,19 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
+jmp start                       ; Go to start
+
+%include "gdt.inc"
+%include "stdio.inc"
+
 start:
     cli
     mov ax, 0x2000
     mov ds, ax
-    mov si, msg_loading
+    mov si, msg_protected_mode
     call puts
 .halt:
     cli
     hlt
 
-%include "stdio.inc"
-
-msg_loading: db 'Preparing to load operating system...', ENDL, 0
+msg_protected_mode: db 'Entering protected mode...', ENDL, 0
